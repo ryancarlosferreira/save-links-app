38318280580623ab8d39ee5252a73341a2301acd
Ryan Carlos <ryancarlosferreira1849@gmail.com>
2025-08-06 22:14:59 -0300

    add router

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
@@ -14,7 +15,7 @@ export default function Index(){
             <View style={styles.header}>
                 <Image source={require("../../assets/logo.png")} style={styles.logo}/>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate("./add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity> 
             </View>
@@ -36,7 +37,7 @@ export default function Index(){
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}> 
                     {/* Toda a área da Modal */}
                     <View style={styles.modalContent}> 
