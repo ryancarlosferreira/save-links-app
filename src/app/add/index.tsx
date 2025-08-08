e407f66b227ec0d4a859185ac0ff78f478bcf127
Ryan Carlos <ryancarlosferreira1849@gmail.com>
2025-08-08 13:20:11 -0300

    add states

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
@@ -10,6 +11,13 @@ import { Categories } from "@/components/categories";
 import { Input } from "@/components/input";
 
 export default function Add() {
+    const [name, setName] = useState("")
+    const [url, setUrl] = useState("")
+
+    function handleAdd() {
+        console.log({ name, url })
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -24,9 +32,9 @@ export default function Add() {
             <Categories />
 
             <View style={styles.form}>
-                <Input placeholder="Nome" />
-                <Input placeholder="Url" />
-                <Button title="Adicionar"/>
+                <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+                <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+                <Button title="Adicionar" onPress={handleAdd} />
             </View>
         </View>
     )
